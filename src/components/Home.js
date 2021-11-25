399c4d46b621d102f55c6540767df6424529f2a3
Dario Perez <darioperez1415@gmail.com>
2021-11-25 00:33:06 -0600

    Added timeline and styling to NavBar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,106 +1,62 @@
-// import React from 'react';
-// import {
-//   VerticalTimeline,
-//   VerticalTimelineElement,
-//   WorkIcon,
-//   SchoolIcon,
-//   StarIcon,
-// } from 'react-vertical-timeline-component';
-// import 'react-vertical-timeline-component/style.min.css';
+import React from 'react';
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from 'react-vertical-timeline-component';
 
-// export default function Home() {
-//   return (
-//     <>
-//       <VerticalTimeline>
-//         <VerticalTimelineElement
-//           className="vertical-timeline-element--work"
-//           contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-//           contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-//           date="2020 - present"
-//           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-//           icon={<WorkIcon />}
-//         >
-//           <h3 className="vertical-timeline-element-title">
-//             Associate Officer Coding Analyst
-//           </h3>
-//           <h4 className="vertical-timeline-element-subtitle">Nashville, TN</h4>
-//           <p>Compliance, Project Management, Team Leading</p>
-//         </VerticalTimelineElement>
-//         <VerticalTimelineElement
-//           className="vertical-timeline-element--work"
-//           date="2019 - 2020"
-//           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-//           icon={<WorkIcon />}
-//         >
-//           <h3 className="vertical-timeline-element-title">Coding Analyst</h3>
-//           <h4 className="vertical-timeline-element-subtitle">Nashville, TN</h4>
-//           <p>Compliance, Project Management, Team Leading</p>
-//         </VerticalTimelineElement>
-//         <VerticalTimelineElement
-//           className="vertical-timeline-element--work"
-//           date="2017 - 2020"
-//           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-//           icon={<WorkIcon />}
-//         >
-//           <h3 className="vertical-timeline-element-title">Pricing Analyst</h3>
-//           <h4 className="vertical-timeline-element-subtitle">
-//             White Plains, NY
-//           </h4>
-//           <p>Valuation Data, Bloomberg API</p>
-//         </VerticalTimelineElement>
-//         <VerticalTimelineElement
-//           className="vertical-timeline-element--work"
-//           date="2006 - 2008"
-//           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-//           icon={<WorkIcon />}
-//         >
-//           <h3 className="vertical-timeline-element-title">VIP Guest Serives</h3>
-//           <h4 className="vertical-timeline-element-subtitle">Brooklyn, NY</h4>
-//           <p>User Experience, Customer Service</p>
-//         </VerticalTimelineElement>
-//         <VerticalTimelineElement
-//           className="vertical-timeline-element--education"
-//           date="November 2021"
-//           iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-//           icon={<SchoolIcon />}
-//         >
-//           <h3 className="vertical-timeline-element-title">
-//             Postman Student Expert
-//           </h3>
-//           <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-//           <p>API</p>
-//         </VerticalTimelineElement>
-//         <VerticalTimelineElement
-//           className="vertical-timeline-element--education"
-//           date="November 2017"
-//           iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-//           icon={<SchoolIcon />}
-//         >
-//           <h3 className="vertical-timeline-element-title">
-//             Bloomberg Certification
-//           </h3>
-//           <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-//           <p>Boomberg Terminal Basics</p>
-//         </VerticalTimelineElement>
-//         <VerticalTimelineElement
-//           className="vertical-timeline-element--education"
-//           date="2011 - 2015"
-//           iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-//           icon={<SchoolIcon />}
-//         >
-//           <h3 className="vertical-timeline-element-title">
-//             Bachelor of Science in Political Science and International Relations
-//           </h3>
-//           <h4 className="vertical-timeline-element-subtitle">
-//             Bachelor Degree
-//           </h4>
-//           <p>Not sure why I didnt this</p>
-//         </VerticalTimelineElement>
-//         <VerticalTimelineElement
-//           iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-//           icon={<StarIcon />}
-//         />
-//       </VerticalTimeline>
-//     </>
-//   );
-// }
+import '../styles/globals/timeline.scss';
+import { ReactComponent as WorkIcon } from '../timeline/work.svg';
+import { ReactComponent as SchoolIcon } from '../timeline/school.svg';
+
+import timelineElements from '../timeline/timelineElements';
+
+import 'react-vertical-timeline-component/style.min.css';
+
+function Home() {
+  const workIconStyles = { background: '#06D6A0' };
+  const schoolIconStyles = { background: '#f9c74f' };
+
+  return (
+    <div>
+      <h1 className="title">The Journey So Far</h1>
+      <VerticalTimeline>
+        {timelineElements.map((element) => {
+          const isWorkIcon = element.icon === 'work';
+          const showButton = element.buttonText !== undefined
+           && element.buttonText !== null
+           && element.buttonText !== '';
+
+          return (
+            <VerticalTimelineElement
+              key={element.key}
+              date={element.date}
+              dateClassName="date"
+              iconStyle={isWorkIcon ? workIconStyles : schoolIconStyles}
+              icon={isWorkIcon ? <WorkIcon /> : <SchoolIcon />}
+            >
+              <h3 className="vertical-timeline-element-title">
+                {element.title}
+              </h3>
+              <h5 className="vertical-timeline-element-subtitle">
+                {element.location}
+              </h5>
+              <p id="description">{element.description}</p>
+              {showButton && (
+                <a
+                  className={`button ${
+                    isWorkIcon ? 'workButton' : 'schoolButton'
+                  }`}
+                  href="/"
+                >
+                  {element.buttonText}
+                </a>
+              )}
+            </VerticalTimelineElement>
+          );
+        })}
+      </VerticalTimeline>
+    </div>
+  );
+}
+
+export default Home;
