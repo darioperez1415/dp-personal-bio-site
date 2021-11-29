94e8d7344ab163af044aa2064171a749114f97ad
Dario Perez <darioperez1415@gmail.com>
2021-11-29 17:17:52 -0600

    Able to send email js functions with form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,77 +1,92 @@
-import React, { useState } from 'react';
-import { createMessage } from '../api/data/messageData';
-import '../styles/globals/contact.scss';
-// // Say Thank you
+import React from 'react';
+import emailjs from 'emailjs-com';
+import styled from 'styled-components';
 
-const initialState = {
-  name: '',
-  email: '',
-  message: '',
-};
-
+const ContactStyle = styled.div`
+  .card-title {
+    max-height: 50px;
+    color: black;
+    font-size: 25px;
+    margin-left: 15px;
+    align-items: center;
+  }
+`;
 export default function ContactForm() {
-  const [formInput, setFormInput] = useState(initialState);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-
-    setFormInput((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const resetForm = () => {
-    setFormInput(initialState);
-  };
-
-  const handleSubmit = (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
-    createMessage({ ...formInput }).then(resetForm);
+
+    emailjs
+      .sendForm(
+        'service_66w1lsr',
+        'template_evxmjdl',
+        e.target,
+        'user_8thHECzkBlKtguZZ732eN',
+      )
+      .then(
+        (result) => {
+          console.warn(result.text);
+        },
+        (error) => {
+          console.warn(error.text);
+        },
+      );
+    e.target.reset();
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          placeholder="Enter your name"
-          value={formInput.name}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Enter your email"
-          value={formInput.email}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="message">Message:</label>
-        <textarea
-          id="message"
-          rows="3"
-          name="message"
-          placeholder="Eenter Message..."
-          value={formInput.message}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div className="m-3">
-        <button type="submit" className="btn btn-success">
-          Submit
-        </button>
-      </div>
-    </form>
+    <div>
+      <ContactStyle>
+        <h5 className="card-title">Leave me a message!</h5>
+        <div className="container">
+          <form onSubmit={sendEmail}>
+            <div className="row pt-5 mx-auto">
+              <div className="col-8 form-group mx-auto">
+                <input
+                  type="text"
+                  className="form-control"
+                  name="name"
+                  placeholder="Name"
+                  required
+                />
+              </div>
+              <div className="col-8 form-group pt-2 mx-auto">
+                <input
+                  type="text"
+                  className="form-control"
+                  name="email"
+                  placeholder="Email Address"
+                  required
+                />
+              </div>
+              <div className="col-8 form-group pt-2 mx-auto">
+                <input
+                  type="text"
+                  className="form-control"
+                  name="subject"
+                  placeholder="Subject"
+                  required
+                />
+              </div>
+              <div className="col-8 form-group pt-2 mx-auto">
+                <textarea
+                  className="form-control"
+                  id=""
+                  cols="30"
+                  row="8"
+                  name="message"
+                  placeholder="Your message"
+                />
+              </div>
+              <div className="col-8 pt-3 mx-auto">
+                <input
+                  type="submit"
+                  className="btn btn-info"
+                  value="Send Message"
+                />
+              </div>
+            </div>
+          </form>
+        </div>
+      </ContactStyle>
+    </div>
   );
 }
